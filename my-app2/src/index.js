eee51e681c6f474ff5116cc565a5e8a8b2802cdf
zhanghuidi <781152188@qq.com>
2021-02-07 10:59:29 +0800

    jsx

---
+++ my-app2/src/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import styles from './app.modules.css'
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import logo from './logo.svg'
+// 对象
+const obj = {
+  firstName: 'zhang',
+  lastName: 'san'
+}
+// 函数
+const formate = (obj) => `${obj.firstName} ${obj.lastName}`
+
+//条件语句
+const showFlag = true
+
+// 循环语句
+const arr = [1, 2, 3]
+// 表达式
+const temp = <p>hahhaha</p>
+const jsx = (<div className={styles.app}>hello,
+    <div>{obj.firstName + ' ' + obj.lastName}</div>
+    <div>{formate(obj)}</div>
+    {showFlag?'3':'4'}
+    {showFlag&&temp}
+    <ul>
+      {arr.map(item=>(
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+    <img src={logo} 
+    // className="logo"
+    className={styles.logo} 
+    style={{width:'50px',height:'50px'}}></img>
+  </div>)
+
+// 1.jsx表达式{}
+// 2.对象
+// 3.函数
+// 4.条件语句
+// 5.循环语句
+// 6.属性
+// 7.模块化
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {jsx}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+console.log(
+'===',styles
+)
