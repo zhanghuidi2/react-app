cead2fda1148d7defebe4d708363710b31e3fc75
zhanghuidi <781152188@qq.com>
2021-02-08 18:03:02 +0800

    自定义hook

--- my-app2/src/page/effectPage.js
+++ my-app2/src/page/effectPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 export default function EffectPage() {
   const [count, setCount] = useState(0)
-  const [date, setDate] = useState(new Date())
+  // const [date, setDate] = useState(new Date())
 
   useEffect(() => {
     // 相当于didMount和didUpdate
@@ -9,6 +9,27 @@ export default function EffectPage() {
     document.title = `count执行了${count}次`
   }, [count]) // 仅仅依赖count
 
+  // useEffect(() => {
+  //   // 相当于didMount和didUpdate
+  //   console.log('useEffect')
+  //   const timer = setInterval(() => {
+  //     setDate(new Date())
+  //   }, 1000)
+  //   return () => clearInterval(timer) // renturn一个函数，相当于willUnmount钩子
+  // }, []) // []代表谁也不依赖，就是didmount的之后执行一次
+  return (
+    <div>
+      <p onClick={()=>setCount(count + 1)}>{count}</p>
+      {/* <p>{date.toLocaleTimeString()}</p> */}
+      <p>{useClock().toLocaleTimeString()}</p>
+    </div>
+  )
+}
+
+// 自定义hook
+// 以use开头
+function useClock() {
+  const [date, setDate] = useState(new Date())
   useEffect(() => {
     // 相当于didMount和didUpdate
     console.log('useEffect')
@@ -17,11 +38,5 @@ export default function EffectPage() {
     }, 1000)
     return () => clearInterval(timer) // renturn一个函数，相当于willUnmount钩子
   }, []) // []代表谁也不依赖，就是didmount的之后执行一次
-  
-  return (
-    <div>
-      <p onClick={()=>setCount(count + 1)}>{count}</p>
-      <p>{date.toLocaleTimeString()}</p>
-    </div>
-  )
+  return date
 }
